5e4352a2189f075bd32708c5a27856bd4d7a4586
cocoxiaoyue <2503383483@qq.com>
2021-11-04 18:04:07 +0800

    feat:配置路由

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.less';
-import { Button } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-    </div>
-  );
+import React from 'react';
+//HashRouter,支持#识别,http://localhost:8080/#/Page3/
+// import { HashRouter as Router, Route } from 'react-router-dom';
+//单页BrowserRouter
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router >
+        <Routes>
+          <Route exact path="/" component={Home} />
+        </Routes>
+      </Router>
+    )
+
+  }
 }
-
 export default App;
