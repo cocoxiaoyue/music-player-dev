afb35c128d82892b5ca5f36abb0f97cd04bcd05a
cocoxiaoyue <2503383483@qq.com>
2022-02-24 18:05:08 +0800

    commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from "redux";
 
-import home from './homeReducer'
+import home from "./homeReducer";
 
 export default combineReducers({
   home,
-})
+});
--- src/views/Footer/index.js
+++ src/views/Footer/index.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./index.less";
-
 import { UnlockOutlined, LockOutlined } from "@ant-design/icons";
 import AudioPlayer from "@components/audioPlayer";
 
+const mapStateToProps = (state) => {
+  console.log(state.home.number);
+  return {
+    number: state.home.number,
+  };
+};
+const mapDispatchToProps = (dispatch, action) => {
+  console.log(dispatch);
+  console.log(action);
+  return {};
+};
 class Footer extends React.Component {
   state = {
     showFoot: true, // 暂时设置为true
     lockFoot: true, // 暂时设置为true
   };
+
   componentDidMount() {}
   topContnteOver = () => {
     if (!this.state.lockFoot) {
@@ -56,4 +68,4 @@ class Footer extends React.Component {
   }
 }
 
-export default Footer;
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);
